44500fdd5819bb85e44b6ab1e5d32ebc8f4392fd
Matthew Budiman <64542531+mattBman23@users.noreply.github.com>
2021-04-07 23:30:18 -0500

    Time: 84 ms (7.79%), Space: 37.7 MB (98.82%) - LeetHub

--- to-lower-case/to-lower-case.js
+++ to-lower-case/to-lower-case.js
@@ -3,5 +3,13 @@
  * @return {string}
  */
 var toLowerCase = function(str) {
-    return str.toLowerCase();
+    // return str.toLowerCase();
+    
+    let ans = '';
+    
+    for (let i = 0; i < str.length; i++) {
+        ans += str.charCodeAt(i) > 64 && str.charCodeAt(i) < 91 ? String.fromCharCode(str.charCodeAt(i) + 32) : str[i];
+    }
+    
+    return ans;
 };
