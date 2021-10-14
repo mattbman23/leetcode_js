33f02cf478443dc9dde10339985068db8365332e
matthew <matthewbudiman@outlook.com>
2021-10-14 00:04:44 -0500

    Completed maximumDepthOfBinaryTree

---
+++ maximumDepthOfBinaryTree/maxDepth.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var maxDepth = function (root) {
+  if (root === null || root === undefined) {
+    return 0;
+  }
+
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+
+// Runtime 128ms | Memory 41.5 MB
